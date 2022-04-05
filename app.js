ea0671a376f55404831b1cf1072c610edb07ec69
Patricio García Millán <patogarciamillan@gmail.com>
2022-04-04 22:39:43 -0300

    Fix errors

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 require('./database/database')
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var registerRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
+const indexRouter = require('./routes/index');
+const registerRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
-var app = express();
-
-const createUser = require('./routes/users')
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
