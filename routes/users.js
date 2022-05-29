cb045b59f130d9873046de43810b68430c603249
Patricio García Millán <patogarciamillan@gmail.com>
2022-05-28 22:11:19 -0300

    Fix Errors

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,12 @@ route
 route
   .post('/',
     body('email').trim().escape().isEmail().not().isEmpty(),
-    body('email').custom(validateEmail),
     body('userName').trim().escape().isAlphanumeric().isLength({min: 4, max: 10}).not().isEmpty(),
-    body('userName').custom(validateUserName),
     body('name').trim().escape().isAlpha('es-ES', {ignore: ' '}).not().isEmpty().isLength({min: 3, max: 25}),
     body('lastName').trim().escape().isAlpha('es-ES', {ignore: ' '}).not().isEmpty().isLength({min: 3, max: 25}),
     body('password').not().isEmpty().isStrongPassword({minSymbols: 0}),
+    validateEmail,
+    validateUserName,
     createUser
     )
 
